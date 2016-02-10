bfb1fb6836a5271feee22d1ffa873ba935b719ed
EdwardGemson <egemson@skidmore.edu>
2016-02-10 15:59:50 -0500

    working API

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	var wallpaperUrl = $('#wallpaper-id').text();
+	var targetDiv = $('#background-image')
+	targetDiv.css({'background-image': 'url('+wallpaperUrl+')'});
+	$('#wallpaper-id').hide
+});
