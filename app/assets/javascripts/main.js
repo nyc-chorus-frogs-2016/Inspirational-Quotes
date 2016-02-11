8240f7bd6b959b345248b8046526351ebf02d239
EdwardGemson <egemson@skidmore.edu>
2016-02-11 08:55:26 -0500

    complete challenge

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,5 +2,5 @@ $(document).ready(function(){
 	var wallpaperUrl = $('#wallpaper-id').text();
 	var targetDiv = $('#background-image')
 	targetDiv.css({'background-image': 'url('+wallpaperUrl+')'});
-	$('#wallpaper-id').hide
+
 });
